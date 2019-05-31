a0de14984015f16b6e20ba35dd62ac7c188fb69f
NosaPrecious <Okundaye.preciousnosa@gmail.com>
2019-05-31 10:09:00 -0400

    login stuff

--- src/index.js
+++ src/index.js
@@ -1,10 +1,66 @@
-document.addEventListener("DOMContentLoaded", init)
+document.addEventListener("DOMContentLoaded", loginPage)
+
+
+function loginPage(e){
+  let form = document.getElementsByTagName('form')[0]
+  form.addEventListener('submit', loginAccess)
+}
+
+function loginAccess(e){
+  e.preventDefault()
+  let myForm = document.querySelector('form')
+  let username = myForm.getElementsByClassName("input100")[0].value
+   myForm.reset()
+
+if(username){
+   checkUserExist(username)
+ }else{
+   window.location.reload()
+ }
+}
+
+function checkUserExist(name){
+
+let data = {
+  username : name
+}
+
+  fetch("http://localhost:3000/trainers",{
+    method : "POST",
+    headers : {
+      "Content-Type" : "Application/json",
+      "Accepts" : "Application/json"
+    },
+    body : JSON.stringify(data)
+  })
+  .then(resp => resp.json())
+  .then(jsonData => {
+    if(jsonData.error){
+      alert(jsonData.error)
+    }else{
+     console.log(jsonData)
+    document.querySelector('.limiter').style.display = "none"
+    document.querySelector('.navbar').style.display = "block"
+    document.querySelector('main').style.display = "block"
+    init()
+  }
+  })
+}
+
+
 
 function init(){
   setTimeout(function(){
     Pokemon.all.forEach(pokemon => pokemon.assignNature()); }, 3000);
+
+    let logout = document.querySelector('#logout')
+    logout.addEventListener('click', logOut)
+
+      // logOut()
+
   // let promise1 = Promise.resolve(Pokemon.all)
-  Promise.all([Pokemon.fetchPokemon(), Nature.fetchNatures(), Trainer.fetchTrainers()])
+   Promise.all([Pokemon.fetchPokemon(), Nature.fetchNatures(), Trainer.fetchTrainers()])
+
   //shuffle current pokemon based on type
   document.querySelector("#shuffle").addEventListener("click", function() {
 
@@ -33,4 +89,10 @@ function init(){
       Pokemon.fetchType(event.target.innerText)
     })
   })
+}
+
+function logOut(){
+  document.querySelector('.limiter').style.display = "block"
+  document.querySelector('.navbar').style.display = "none"
+  document.querySelector('main').style.display = "none"
 }
